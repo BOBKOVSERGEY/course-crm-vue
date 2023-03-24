1f14d8a3c3db02f9a37809818e3c8774ba0374b8
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2023-03-24 09:07:07 +0300

    bag fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import {ref} from "vue";
 
+const textOne = ref('Привет');
+const textTwo = ref('из моего');
+const textTree = ref('первого приложения на Vue3');
 </script>
 
 <template>
-  <button class="button">hello, world, кнопка</button>
+  <button class="button">{{textOne}} {{textTwo}} {{textTree}}</button>
 </template>
