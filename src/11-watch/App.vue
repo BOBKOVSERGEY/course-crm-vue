8a9caa61a3d0f61e399e8ede1b0dfd6458d77d52
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2023-04-21 13:21:08 +0300

    11-hw-computed-watch-watcheffect

---
+++ src/11-watch/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app__wrapper app__wrapper_page-type_auth">
+    <a class="logo logo_size_l"><img src="@/assets/i/logo/logo-dark.svg" alt="OpenCrm"></a>
+    <div class="form-auth">
+      <div class="form-auth__main">
+        <div class="textbox">
+          <label class="textbox__wrapper">
+            <div class="textbox__label">
+              Code from sms
+            </div>
+            <input class="textbox__input"
+                   type="number"
+                   placeholder="****"
+                   v-model="code"
+            >
+          </label>
+          <div class="textbox__message">
+            {{ message }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+
+import {reactive, ref, watch} from "vue";
+
+const code = ref();
+const message = ref('Input code from sms');
+
+const a = reactive({
+  name: 'N',
+  code: 1234
+})
+
+watch(a, (newVal, oldVal) => {
+  console.log(newVal.subfield)
+  if (newVal.subfield == oldVal.subfield) {
+    alert('Вот так Vue3')
+  }
+})
+
+watch(code, (newVal, oldValue) => {
+  if (newVal.toString().length === 4) {
+    message.value = 'Идет отправка...'
+    code.value = '';
+  }
+})
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from '@/11-computed/App.vue'
+import App from '@/11-hw-watch-watch-effect-computed/App.vue'
 
 import '@/assets/sass/app.scss'
 
